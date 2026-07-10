aa54aa4df25e6081f0736d1dd4445e1c9980f99a
Adithya Boyapati <Boyapatiadithya@gmail.com>
2026-07-10 13:21:00 +0530

    sumofdigits.c

--- digitbased/sumofdigits.c
+++ digitbased/sumofdigits.c
@@ -1,11 +1,13 @@
 #include<stdio.h>
 int main(){
-    int n,r,sum=0;
+    int n, sum=0,r=0;
     printf("enter the value of n:");
-    scanf("%d",&n);
-    while(n!=0){
-     r=n%10;
-     sum=sum+r;
-     n=n/10;
-    } printf("sum:%d",sum);
+    scanf("%d", &n);
+    while(n>0){
+        r=n%10;
+        sum+=r;
+        n/=10;
+    }
+    printf("sum: %d", sum);
+    return 0;
 }
