4fdf56d7bf327beb90d16d955494f11b866cc6bc
Adithya Boyapati <Boyapatiadithya@gmail.com>
2026-07-11 11:01:27 +0530

    firstlargest.c

--- digitbased/firstlargest.c
+++ digitbased/firstlargest.c
@@ -1,15 +1,15 @@
 #include<stdio.h>
 int main()
 {
-    int n,large=0,r;
+    int n,r,first=0;
     printf("Enter a number: ");
     scanf("%d",&n);
     while(n>0)
     {
         r=n%10;
-        if(r>large)
-            large=r;
+        if(r>first)
+        first=r;
         n/=10;
     }
-    printf("first highest number: %d",large);
+    printf("first highest number: %d",first);
 }
