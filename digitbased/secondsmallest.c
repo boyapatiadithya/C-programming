014d306c5bab10fec9b07c0b880efc7b01a78898
Adithya Boyapati <Boyapatiadithya@gmail.com>
2026-07-11 10:44:32 +0530

    secondsmallest.c

--- digitbased/secondsmallest.c
+++ digitbased/secondsmallest.c
@@ -1,23 +1,23 @@
 #include <stdio.h>
 int main() {
-    int n, r, fs= 9, ss = 10;
+    int n, r, first= 9, second = 10;
     printf("enter the value of n:");
     scanf("%d", &n);
     while (n > 0) {
         r = n % 10;
-        if (r < fs) {
-            ss=fs;
-            fs= r;
+        if (r < first) {
+            second=first;
+            first= r;
         }
-        else if (r > fs && r < ss)
-            ss = r;
+        else if (r > first && r < second)
+            second = r;
         n /= 10;
     }
-    printf("First smallest: %d\n", fs);
-    if (ss == 10)
+    printf("First smallest: %d\n", first);
+    if (second == 10)
         printf("Second smallest doesn't exist");
     else
-        printf("Second smallest: %d", ss);
+        printf("Second smallest: %d", second);
 
     return 0;
 }
