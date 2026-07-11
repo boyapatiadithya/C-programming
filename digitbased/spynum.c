bbf2a256bbe4fcd1fa68ebb2adbd204735e3c65d
Adithya Boyapati <Boyapatiadithya@gmail.com>
2026-07-11 11:35:38 +0530

    spynum.c

--- digitbased/spynum.c
+++ digitbased/spynum.c
@@ -1,15 +1,17 @@
 #include<stdio.h>
-int main(){
-    int n, r, sum=0, product=1;
+int main() {
+    int n, r, sum = 0, product = 1;
     printf("Enter a number: ");
-    scanf("%d",&n);
-    while(n>0){
-        r=n%10;
-        sum+=r;
-        product*=r;
-        n/=10;
+    scanf("%d", &n);
+    while(n > 0)
+    {
+        r = n % 10;
+        sum += r;
+        product *= r;
+        n /= 10;
     }
-    (sum==product)?printf("spy number"):printf("not spy number");
+    (sum == product) ? printf("Spy Number"): printf("Not Spy Number");
+    return 0;
 }
 //(whose sum of digits equals the product of digits.)
-//n=9-->3*3
+//(Number:22	Sum of digits:2+2;	Product of digits:2*2)	
